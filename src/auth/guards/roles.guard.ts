39ee90ee61c394cedccc452f40bd060d301e6fc0
Davi Nobre <davi.nbtec@gmail.com>
2026-05-08 18:02:34 -0300

    feat: implementar guards para controle de acesso

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,42 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+
+import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { AuthUser, UserRole } from '../types/jwt-payload.interface';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = request.user as AuthUser;
+
+    if (!user) {
+      throw new ForbiddenException();
+    }
+
+    const hasRole = requiredRoles.includes(user.role);
+
+    if (!hasRole) {
+      throw new ForbiddenException();
+    }
+
+    return true;
+  }
+}
