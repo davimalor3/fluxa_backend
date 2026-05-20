e2196f240fc645ba9faad5e88f07969299bbca4d
Davi Nobre <davi.nbtec@gmail.com>
2026-05-20 01:34:32 -0300

    feat: adicionar validações ao CreateTableDto e atualizar UpdateTableDto para herdar as novas propriedades

--- src/tables/dto/create-table.dto.ts
+++ src/tables/dto/create-table.dto.ts
@@ -1,1 +1,12 @@
-export class CreateTableDto {}
+import { IsInt, IsOptional, IsString, MaxLength, Min } from 'class-validator';
+
+export class CreateTableDto {
+  @IsInt()
+  @Min(1)
+  numero!: number;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  observacao?: string;
+}
