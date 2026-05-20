fee7e8d6b3ea839d1e7bb88488b682a3faaabd3a
Davi Nobre <davi.nbtec@gmail.com>
2026-05-20 01:34:44 -0300

    feat: implementar método de criação de mesa com validação de existência e tratamento de exceções

--- src/tables/tables.service.ts
+++ src/tables/tables.service.ts
@@ -1,26 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 import { CreateTableDto } from './dto/create-table.dto';
-import { UpdateTableDto } from './dto/update-table.dto';
 
 @Injectable()
 export class TablesService {
-  create(createTableDto: CreateTableDto) {
-    return 'This action adds a new table';
+  constructor(private prisma: PrismaService) {}
+
+  // --------- cria uma nova mesa para restaurante especifico ---------------
+  async create(dto: CreateTableDto, restaurante_id: string) {
+    // Verifica se o número da mesa já existe para este restaurante específico
+    const mesaExistente = await this.prisma.mesas.findUnique({
+      where: {
+        numero_restaurante_id: {
+          numero: dto.numero,
+          restaurante_id,
+        },
+      },
+    });
+
+    if (mesaExistente) {
+      throw new BadRequestException(
+        `A mesa número ${dto.numero} já está cadastrada.`,
+      );
+    }
+
+    return this.prisma.mesas.create({
+      data: {
+        numero: dto.numero,
+        observacao: dto.observacao,
+        restaurante_id,
+        status: 'DISPONIVEL',
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all tables`;
+  async findAll(restaurante_id: string) {
+    return this.prisma.mesas.findMany({
+      where: { restaurante_id, deleted_at: null },
+      orderBy: { numero: 'asc' },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} table`;
-  }
+  async findOne(id: string, restaurante_id: string) {
+    const mesa = await this.prisma.mesas.findFirst({
+      where: { id, restaurante_id, deleted_at: null },
+    });
 
-  update(id: number, updateTableDto: UpdateTableDto) {
-    return `This action updates a #${id} table`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} table`;
+    if (!mesa) {
+      throw new NotFoundException('Mesa não encontrada.');
+    }
+    return mesa;
   }
 }
