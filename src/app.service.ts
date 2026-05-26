64f1d632f9f462a9282a33d2a61441bb73e53b5d
Davi Nobre <davi.nbtec@gmail.com>
2026-05-26 19:54:26 -0300

    fix: mensagem de saudação do método getHello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<br><br><br><br><br><br>======== ONLINE PIVETÃO! =======';
   }
 }
